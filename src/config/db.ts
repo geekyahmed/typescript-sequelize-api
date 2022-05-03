6fd26ebdc9192951a68b7f16bb9cc01b8fde2912
Ahmed Bankole <thegeekyahmed@gmail.com>
2022-05-02 19:23:12 -0700

    add user model

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,9 @@ import { db_host, db_port, db_name, db_user, db_password } from './index';
 
 export default new Sequelize({
     dialect: "postgres",
-    host: db_host,
-    port: db_port,
-    database: db_name,
-    username: db_user,
-    password: db_password
+    host: 'localhost',
+    port: 5432,
+    database: 'mypg',
+    username: 'postgres',
+    password: 'pa$$word22'
 })
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from 'express'
+import User, { UserMap } from '../models/User'
+import db from '../config/db'
 
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    const results: String[] = []
+    UserMap(db)
+    const results = await User.findAll()
 
     res.status(200).json({
         message: "Success",
